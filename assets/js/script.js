29c358fd5a07b3c137c35ff60d74f0df49a05429
Mauricio Campos <thoyferr@gmail.com>
2022-03-10 00:04:20 -0300

    voceto listo sin validador

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ btBuscar.addEventListener('click', () => {
 
 
             ficha(heroe);
-
+            crearGrafico(heroe);
 
         }
 
@@ -20,7 +20,7 @@ btBuscar.addEventListener('click', () => {
 
 
 const ficha = (sHeroe) => {
-
+    let estado;
     let nombCompleto = sHeroe.biography['full-name'];
     let altEgos = sHeroe.biography['alter-egos'];
     let aliados = sHeroe.biography['aliases'];
@@ -36,45 +36,119 @@ const ficha = (sHeroe) => {
     let pelo = sHeroe.appearance['hair-color'];
     let ocupacion = sHeroe.work['occupation'];
     let base = sHeroe.work['base'];
-
-    if (nacimiento==="-"){
+    let afili = sHeroe.connections['group-affiliation'];
+    let familia = sHeroe.connections['relatives'];
+    if (aliado === "bad"){
+        estado = "Villano";
+    }else{
+        estado = "Heroe";
+    }
+    if (nacimiento === "-") {
         nacimiento = "sin lugar de nacimiento";
     }
-    if (nombCompleto === ""){
+    if (nombCompleto === "") {
         nombCompleto = sHeroe.name;
     }
+    if (ocupacion  === "-") {
+        ocupacion  = "Sin Ocupacion";
+    }
+    if (base  === "-") {
+        base  = "Sin base de operaciones";
+    }
+    if (familia ==="-"){
+        familia = "Sin Familia Conocida"
+    }
 
-    $('#seccionheroe').html(`<div class="container col-12 col-sm-11 text-center my-5">
-    <div class="row  pt-2"> <div class="card  col-12 col-sm-7 mb-3"><div class="row no-gutters">
-                <div  class="col-md-5 align-self-center"><img src="${sHeroe.image.url}" class="imgsecu mt-2" >
+    $('#seccionheroe').html(`<div class="container col-12 col-sm-10 text-center my-4 ">
+    <div class="row  pt-2"> <div class="card  col-12 col-sm-7 mb-2"><div class="row no-gutters">
+                <div  class="col-md-5 align-self-center bg-transparent"><img src="${sHeroe.image.url}" class="imgsecu mt-2" ><p class="text-center text-comic-2">${estado} N°${sHeroe.id}</p>
                 </div> <div class="col-md-7 "> <div id="ficha" class="card-body"> </div></div>
-                </div></div><div class="col-12 col-sm-5 mb-3 "> <p class="card-text">Aqui va el grafio </p> </div></div>`);
-
-
-
+                </div></div><div class="col-12 col-sm-5 mb-2 " style="height: 100%; width: 100%;"> <div id="graficoheroe" style="height: 100%; width: 100%;"></div>..  </div></div>`);
     $('#ficha').html(`<h5 class=" text-comic-1 text-center">${sHeroe.name}<hr></h5>
-    <p class="text-comic-3 ml-1 text-left"> <b>Nombre Completo:</b> ${nombCompleto}<br> <b>Alter-ego: </b>${altEgos}<br><b>Alias:</b> ${aliados}<br>
-     <b>Lugar de nacimiento:</b> ${nacimiento}<br><b>Primera aparicion: </b>${aparicion} <br><b> Editora: </b>${editor} <br>
-     <b>Afiliacion: </b>${aliado}<br><hr></p>
-     <p class="text-comic-2 text-left" ><b>&nbsp;Caracteristicas fisicas<b></p>
-     <p class="text-comic-3 text-left"><b>Genero: </b>${genero}<br><b>Raza :</b>${raza  } </p>
-     `);
+    <dl class="row text-comic-3">
+    <dt class="col-sm-6 text-left">Nombre Completo </dt><dd class="col-sm-6 text-left">${nombCompleto}</dd>
+    <dt class="col-sm-6 text-left">Alter-ego</dt><dd class="col-sm-6 text-left">${altEgos}</dd>
+    <dt class="col-sm-6 text-left">Alias</dt><dd class="col-sm-6 text-left">${aliados}</dd>
+    <dt class="col-sm-6 text-left">Lugar de nacimiento</dt><dd class="col-sm-6 text-left">${nacimiento}</dd>
+    <dt class="col-sm-6 text-left">Primera aparicion</dt><dd class="col-sm-6 text-left">${aparicion}</dd>
+    <dt class="col-sm-6 text-left">Editora</dt><dd class="col-sm-6 text-left">${editor}</dd>
+    <dt class="col-sm-6 text-left">Afiliacion</dt><dd class="col-sm-6 text-left">${aliado}</dd>
+    <dt class="col-sm-12"><hr></dt><dd class="col-sm-0"></dd> 
+    <dt class="col-sm-0"><br></dt><dd class="col-sm-12">
+        <dl class="row">
+            <dt class="col-sm-2 text-left">Sexo</dt>
+            <dd class="col-sm-4 text-left">${genero}</dd>
+            <dt class="col-sm-2 text-left">Raza</dt>
+            <dd class="col-sm-4 text-left">${raza}</dd>
+            <dt class="col-sm-2 text-left">Ojos</dt>
+            <dd class="col-sm-4 text-left">${ojos}</dd>
+            <dt class="col-sm-2 text-left">Pelo</dt>
+            <dd class="col-sm-4 text-left">${pelo}</dd>      
+        </dl>
+    </dd>
+    <dt class="col-sm-3 text-left">Altura</dt><dd class="col-sm-9 text-left">${altura}</dd>
+    <dt class="col-sm-3 text-left">Peso</dt><dd class="col-sm-9 text-left">${peso}</dd>
+    <dt class="col-sm-12"><hr></dt><dd class="col-sm-0"></dd> 
+    <dt class="col-sm-3 text-left ">Ocupacion</dt><dd class="col-sm-9 text-justify">${ocupacion}</dd>
+    <dt class="col-sm-3 text-left">Base</dt><dd class="col-sm-9 text-justify">${base}</dd>
+    <dt class="col-sm-12"><hr></dt><dd class="col-sm-0"></dd> 
+    <dt class="col-sm-3 text-left ">Afiliacion</dt><dd class="col-sm-9 text-justify">${afili}</dd>
+    <dt class="col-sm-3 text-left">Familia</dt><dd class="col-sm-9 text-justify">${familia}</dd> 
+    </dl> `);
+    
+    
+}
+
+const crearGrafico = (graHeroe) => {
+    let nombre ="Estadisticas";
+    let intel = graHeroe.powerstats['intelligence'];
+    let estra = graHeroe.powerstats['strength'];
+    let velocidad =  graHeroe.powerstats['speed'];
+    let durabilidad = graHeroe.powerstats['durability'];
+    let poder = graHeroe.powerstats['power'];
+    let combate = graHeroe.powerstats['combat'];
+    
+
+    if (intel  ==="null"){
+        intel = 0;
+    }
+    if (estra  ==="null"){
+        estra = 0;
+    }
+    if (velocidad  ==="null"){
+        velocidad = 0;
+    }
+    if (durabilidad  ==="null"){
+        durabilidad = 0;
+    }
+    if (poder  ==="null"){
+        poder = 0;
+    }
+    if (combate  ==="null"){
+        combate = 0;
+    }
 
 
-
-    //"appearance":{
-    //   "gender":"Male",
-    //   "race":"Mutant",
-    //   "height":["6'2","188 cm"],
-    //   "weight":["210 lb","95 kg"],
-    //   "eye-color":"Brown",
-    //   "hair-color":"No Hair"},
-    //"w/ork":{"
-    //   occupation":"Mercenary; former enforcer, government operative, sumo wrestler, soldier, assassin, anti-hero, others",
-    //    "base":"Cavern-X, Sedona, Arizona, Mobile"},
-    //"connections":{"
-    //    group-affiliation":"Thunderbolts (Strike Team), shares body with Agent Preston; formerly X-Force, Deadpool Corps, Agency X, S.H.I.E.L.D.; Code Red, Six Pack, One World Church, DP Inc., Weapon X, Weapon Plus, Heroes for Hire, Secret Defenders, Frightful Four, Team Deadpool, L",
-    //   "relatives":"Thomas \"Mickey\" Wilson (father, deceased); Hailey Wilson (mother, deceased); Gretchen Wilson, Orksa (ex-wives); Evil Deadpool (clone); Widdle Wade (clone)"},
-
-
+    let grafico = new CanvasJS.Chart("graficoheroe", {
+        theme: "light2", // "light2", "dark1", "dark2"
+        animationEnabled: true, 		
+        title: {
+            text: nombre
+        },
+        data: [
+            {
+                type: "pie",
+                dataPoints: [
+                    { label: "Inteligencia", y: intel },
+                    { label: "Estrategia", y: estra },
+                    { label: "Velocidad", y: velocidad },
+                    { label: "Dureza", y: durabilidad },
+                    { label: "Poder", y: poder},
+                    { label: "Poder", y: combate }
+                    
+                ]
+            }
+        ]
+    });
+    grafico.render();
 }
